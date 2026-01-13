78697839615a6a2b8d0f6dbe6484c2839480cb3e
bjjcartagena <49793798+bjjcartagena@users.noreply.github.com>
2026-01-13 13:04:16 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import Dashboard from "./screens/Dashboard";
-import Garage from "./screens/Garage";
-import TaskDetail from "./screens/TaskDetail";
-import VehicleTypeSelection from "./screens/VehicleTypeSelection";
-import VehicleProfileSetup from "./screens/VehicleProfileSetup";
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import VehicleTypeSelection from './pages/VehicleTypeSelection';
+import VehicleProfileSetup from './pages/VehicleProfileSetup';
+import Dashboard from './pages/Dashboard';
+import TaskDetail from './pages/TaskDetail';
+import Garage from './pages/Garage';
+import History from './pages/History';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<VehicleTypeSelection />} />
-      <Route path="/setup-profile" element={<VehicleProfileSetup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/garage" element={<Garage />} />
-      <Route path="/task-detail" element={<TaskDetail />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<VehicleTypeSelection />} />
+        <Route path="/setup-profile" element={<VehicleProfileSetup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/task-detail" element={<TaskDetail />} />
+        <Route path="/garage" element={<Garage />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
